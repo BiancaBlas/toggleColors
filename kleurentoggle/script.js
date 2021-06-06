50e52df5347316a9fd9c301b1dbef083b92ed45a
Bianca Blasweiler-Butter <84021462+BiancaBlas@users.noreply.github.com>
2021-06-06 15:02:09 +0200

    Add files via upload

---
+++ kleurentoggle/script.js
@@ -0,0 +1,68 @@
+const menuButton = document.querySelector("#menu");
+
+const getSidebar = document.querySelector(".colorChanger");
+const getSidebarLi = document.querySelectorAll(".colorChanger li");
+const arrayLength = getSidebarLi.length;
+const body = document.body;
+const div = document.querySelector(".text");
+const navBar = document.querySelector(".navBar");
+const inputButtons = document.querySelectorAll("input");
+
+// const toggleMenu = function () {
+//     getSidebar.classList.toggle("show");
+// };
+//met toggle krijg ik de functie niet helemaal werkend zoals ik zou willen, waardoor ik altijd alsnog 2 funties nodig had, dus dan liever zonder toggle
+
+const getMenu = function () {
+    getSidebar.classList.add("show");
+};
+const removeMenu = function () {
+    getSidebar.classList.remove("show");
+};
+menuButton.addEventListener("mouseover", getMenu);
+getSidebar.addEventListener("mouseover", getMenu);
+navBar.addEventListener("mouseleave", removeMenu);
+
+//met toggle krijg je als je 1 kleur klikt en daarna de ander en daarna nog een keer die ander, dat je de vorige kleur terug krijgt
+//ik wil dat alles grijs wordt ongeacht welke kleuren ik van te voren in heb geklikt, dus dan maar zo
+
+inputButtons.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const li = e.target.parentElement.className;
+        if (body.className == li) {
+            body.removeAttribute("class");
+            div.textContent = "geen kleur gekozen";
+            div.removeAttribute("class");
+            // het lukt me niet om ook de checked status van de radiobutton te verwijderen, meerdere manieren geprobeerd, maar ik vind het wel ok om te zien wat de laatste toggle is geweest
+        } else {
+            body.removeAttribute("class");
+            body.classList.add(li);
+            div.textContent = e.target.parentElement.textContent;
+            div.classList.add("smallerdiv");
+        }
+        removeMenu();
+        //met toggle krijg je als je 1 kleur klikt en daarna de ander en daarna nog een keer die ander, dat je de vorige kleur terug krijgt
+        //ik wil dat alles grijs wordt ongeacht welke kleuren ik van te voren in heb geklikt, dus dan maar zo
+    });
+});
+
+document.addEventListener("keydown", function (event) {
+    let number = parseInt(event.key) - 1;
+    let color = getSidebarLi[number].className;
+    let text = getSidebarLi[number].textContent;
+
+    if (body.className == color) {
+        body.removeAttribute("class");
+        div.textContent = "geen kleur gekozen";
+        div.removeAttribute("class");
+    } else {
+        body.removeAttribute("class");
+        body.classList.add(color);
+        div.textContent = text;
+        div.classList.add("smallerdiv");
+    }
+    removeMenu();
+    // ook hier lukt het me niet om dan ook de radiobuttons aan of uit te vinken
+});
+
+// Als gebruiker wil ik het menu langzaam in beeld zien schuiven en weer uit beeld zien schuiven, in plaats van plotseling verschijnen (doe dit met CSS transitions). (de richting maakt niet uit!)
